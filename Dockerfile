bb2f7292ba8e32f7623f00a83c61ddb37eabf528
KanHatakeyama <yaj_k@msn.com>
2022-03-16 19:16:10 +0900

    docker

---
+++ Dockerfile
@@ -0,0 +1,38 @@
+FROM continuumio/miniconda3
+
+RUN conda create -n chemodel python==3.7.9
+
+SHELL ["conda", "run", "-n", "chemodel", "/bin/bash", "-c"]
+
+RUN conda install django-heroku -c conda-forge --override-channels
+
+RUN pip3 install torch==1.8.1 torchvision==0.9.1 torchaudio==0.8.1
+RUN pip3 install jupyter
+RUN pip3 install pandas==1.2.4
+RUN pip3 install joblib==1.1.0
+#RUN conda install -c rdkit rdkit==2021.03.1 --override-channels
+RUN conda install -c rdkit -c conda-forge rdkit --override-channels
+RUN pip3 install tqdm==4.60.0
+RUN pip3 install scikit-learn==0.24.2
+RUN pip3 install pyyaml
+RUN pip3 install dgl==0.6.1
+RUN pip3 install mordred==1.2.0
+RUN pip3 install openpyxl==3.0.5
+#RUN pip3 install Django
+RUN pip3 install django-pandas==0.6.6
+RUN pip3 install django-ckeditor
+RUN pip3 install django-import-export
+RUN pip3 install django-admin-numeric-filter==0.1.6
+RUN pip3 install gunicorn
+
+
+#this should be after django-heroku
+RUN pip3 install Django==3.2.12
+
+RUN mkdir /code
+WORKDIR /code
+ADD . /code
+
+RUN pip3 list
+CMD python manage.py runserver
+#CMD gunicorn --bind 0.0.0.0:$PORT config.wsgi
--- ion_manager/views.py
+++ ion_manager/views.py
@@ -1,53 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .forms import UploadFileForm
-import sys
-from django.http import HttpResponseRedirect
-#from .ion_predictor.django_wrapper.auto_predictor import screen_predict
 import os
 
 # Create your views here.
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    return HttpResponse("Hello, world.")
 
 
-"""
-
-# ------------------------------------------------------------------
-def file_upload(request):
-    if request.method == 'POST':
-        form = UploadFileForm(request.POST, request.FILES)
-        if form.is_valid():
-            handle_uploaded_file(request.FILES['file'])
-            file_obj = request.FILES['file']
-            path="media/documents/"+file_obj.name
-
-            #calculate 
-            df=screen_predict(path)
-
-            #remove file
-            try:
-                os.remove(path)
-            except:
-                pass
-
-            #return csv
-            response = HttpResponse(content_type='text/csv; charset=utf8')
-            response['Content-Disposition'] = 'attachment; filename=users.csv'
-            df.to_csv(path_or_buf=response, encoding='utf_8_sig', index=None)
-            return response 
-
-    else:
-        form = UploadFileForm()
-    return render(request, 'ion_manager/upload.html', {'form': form})
-#
-#
-# ------------------------------------------------------------------
-def handle_uploaded_file(file_obj):
-    file_path = 'media/documents/' + file_obj.name 
-    with open(file_path, 'wb+') as destination:
-        for chunk in file_obj.chunks():
-            destination.write(chunk)
-"""
